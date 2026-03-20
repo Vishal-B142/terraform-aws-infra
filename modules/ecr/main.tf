ccc6568e8018fec6bccee875b33ceb31933552a6
Vishal-B142 <44637144+Vishal-B142@users.noreply.github.com>
2026-03-20 18:00:06 +0530

    Create main.tf

---
+++ modules/ecr/main.tf
@@ -0,0 +1,44 @@
+resource "aws_ecr_repository" "this" {
+  name                 = var.repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last ${var.keep_tagged_count} tagged images"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["v", "sha"]
+          countType     = "imageCountMoreThan"
+          countNumber   = var.keep_tagged_count
+        }
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 2
+        description  = "Remove untagged images after 7 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 7
+        }
+        action = { type = "expire" }
+      }
+    ]
+  })
+}
